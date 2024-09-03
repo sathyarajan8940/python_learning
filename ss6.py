976fea56327c619c016de781582e6fb92599ceea
sathyarajan8940 <sathyasathya51508@gmail.com>
2024-09-03 12:08:48 +0530

    Add files via upload

---
+++ ss6.py
@@ -0,0 +1,5 @@
+p=float(input("Enter the principal amout:"))
+r=float(input("Enter the rate of interest:"))
+t=float(input("Enter the time period:"))
+interest=(p*r*t)/100
+print("Simple Interest:",interest)
